c6255a841e47df72e2dcf990ffe89147666760f4
Nidvar <djayshine87@hotmail.com>
2020-12-25 19:17:04 +1100

    reusable functions, added 2 inputs, comparing two movies coming along

--- script.js
+++ script.js
@@ -1,47 +1,25 @@
 const api_key = '8d9d75cd'
 console.log(movie_data)
 
-const movie_list = async (search)=>{
-    const response = await axios.get(`http://www.omdbapi.com/`,{
-        params:{
-            apikey: api_key,
-            s: search
-        }
-    })
-    if(search==''){
-        return []
-    }
-    if(response.data.Response == 'False'){
-        return [{Title: 'Movie not found', Poster:''}]
-    }else{
-        console.log(response.data.Search)
-        return response.data.Search
-    }
-}
-
-
-const search_function = (user_input, element_to_display_list,element_to_display_single_movie)=>{
+const search_function = (user_input, element_to_display_list, element_to_display_single_movie)=>{
     const execute_search =  async (e)=>{
 
         element_to_display_list.innerHTML = ''
 
         const response = await movie_list(e.target.value)
+
         
                         for(let item of response){
+
                             const div = document.createElement('div')
-                            div.innerHTML = `
-                                <div class="dropdown-content">
-                                    <a href="#" class="dropdown-item" id='${item.imdbID}'>
-                                        <img class='dropdown_image' src='${item.Poster=='N/A'?'':item.Poster}'> ${item.Title}
-                                    </a>
-                                </div>
-                            `
+
+                            div.innerHTML = render_list(item);
+
                             element_to_display_list.appendChild(div)
 
-                            document.getElementById(`${item.imdbID}`).addEventListener('click',(e)=>{
+                            div.addEventListener('click',(e)=>{
                                 
                                 display_chosen_movie(element_to_display_single_movie, item.imdbID)
-
 
                                 user_input.value = `${item.Title}`;
                                 element_to_display_list.innerHTML=''
@@ -49,14 +27,17 @@ const search_function = (user_input, element_to_display_list,element_to_display_
                     
                         }
 
-
-
-
     }
     user_input.addEventListener('input', timeoutDelayFunction(execute_search, 1000))
 }
 
+
+
 search_function(document.getElementById('movie_search'), document.getElementById('movie_list'), document.getElementById('chosen_movie'))
+
+search_function(document.getElementById('movie_search2'), document.getElementById('movie_list2'), document.getElementById('chosen_movie2'))
+
+
 
 document.querySelector('body').addEventListener('click', (e)=>{
     if(!document.getElementById('movie_list').contains(e.target)){
--- utilities.js
+++ utilities.js
@@ -10,7 +10,23 @@ const timeoutDelayFunction = (any_func, delay)=>{
     }
 }
 
-
+const movie_list = async (search)=>{
+    const response = await axios.get(`http://www.omdbapi.com/`,{
+        params:{
+            apikey: api_key,
+            s: search
+        }
+    })
+    if(search==''){
+        return []
+    }
+    if(response.data.Response == 'False'){
+        return [{Title: 'Movie not found', Poster:''}]
+    }else{
+        console.log(response.data.Search)
+        return response.data.Search
+    }
+}
 
 const display_chosen_movie = async (destination_element, imdb_movieid)=>{
     const response = await axios.get(`http://www.omdbapi.com/`,{
@@ -22,16 +38,29 @@ const display_chosen_movie = async (destination_element, imdb_movieid)=>{
     const result = response.data
 
     destination_element.innerHTML = `
-    <div>
-        <h2>${result.Title} (${result.Year})</h2>
-        <p>${result.Runtime} | ${result.Genre} | ${result.Released}</p>
+    <div class='dualbox'>
         <img class='single_image' src='${result.Poster}'>
-        <p>${result.Plot}</p>
-        <p><strong>Director:</strong> ${result.Director}</p>
-        <p><strong>Actors:</strong> ${result.Actors}</p>
-        <p><strong>Writers:</strong> ${result.Writer}</p>
-        <p><strong>Awards:</strong> ${result.Awards}</p>
+        <div>
+            <h3>${result.Title} (${result.Year})</h3>
+            <p>${result.Runtime} | ${result.Genre} | ${result.Released}</p>
+            <p>${result.Plot}</p>
+        </div>
     </div>
+            <p><strong>Director:</strong> ${result.Director}</p>
+            <p><strong>Actors:</strong> ${result.Actors}</p>
+            <p><strong>Writers:</strong> ${result.Writer}</p>
+            <p><strong>Awards:</strong> ${result.Awards}</p>
+        
     `
 }
 
+const render_list = (item)=>{
+    return (`
+            <div class="dropdown-content">
+                <a href="#" class="dropdown-item name_from_list" id='${item.imdbID}'>
+                    <img class='dropdown_image' src='${item.Poster=='N/A'?'':item.Poster}'> 
+                    <p class='titlespan'>${item.Title} (${item.Year})</p>
+                </a>
+            </div>
+        `)
+}
